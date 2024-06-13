5ad35cb5c55cb7cbd63a359d28c3016dc00c4223
amenzedev <amageb2014@gmail.com>
2024-06-13 00:19:45 -0400

    added newyear project without css

--- CS50_PY_JS/Lecture2/Lecture4/Django_tut/hello/views.py
+++ CS50_PY_JS/Lecture2/Lecture4/Django_tut/hello/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!");
+    return  render(request,"hello/index.html")
 
 def trial(request):
     return HttpResponse("Hello, trial!");
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return  render(request, "hello/greet.html", {
+        "name":name.capitalize()
+    })
