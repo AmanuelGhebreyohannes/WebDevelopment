a2ea914d2c22267a1ff692851134e44998c6ab37
amenzedev <amageb2014@gmail.com>
2024-06-14 01:53:35 -0400

    partial search method is implemented

--- CS50_PY_JS/Project1/wiki/encyclopedia/urls.py
+++ CS50_PY_JS/Project1/wiki/encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("searchResult", views.search, name="search"),
     path("<str:title>", views.title, name="title"),
+     
 ]
--- CS50_PY_JS/Project1/wiki/encyclopedia/views.py
+++ CS50_PY_JS/Project1/wiki/encyclopedia/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
 from . import util
-
+from . import forms
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "searchForm":forms.SearchForm
     })
 
 def title(request,title):
@@ -16,6 +17,35 @@ def title(request,title):
     return render(request, "encyclopedia/title.html", {
         "title": title,
         "description":description,
-        "page_available":page_available
+        "page_available":page_available,
+        "searchForm":forms.SearchForm
     })
+
+
+
+def search(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = util.get_entry(title)
+        page_available = True
+        if(description == None):
+            page_available = False
+        new_entries = []
+        if(page_available == False):
+            for entry in util.list_entries:
+                if(title in entry):
+                    new_entries.append(entry)
+            return render(request, "encyclopedia/index.html", {
+                "entries": new_entries,
+                "searchForm":forms.SearchForm
+            })
+            
+
+        return render(request, "encyclopedia/title.html", {
+            "title": title,
+            "description":description,
+            "page_available":page_available,
+            "searchForm":forms.SearchForm
+        })
+
 
