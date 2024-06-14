e3d76fe33cf435fec24b541e2ad8aa72f241cd0c
amenzedev <amageb2014@gmail.com>
2024-06-13 23:50:48 -0400

    added Project 1 starter code and implementatino of entry page

---
+++ CS50_PY_JS/Project1/wiki/encyclopedia/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def title(request,title):
+    description = util.get_entry(title)
+    page_available = True
+    if(description == None):
+        page_available = False
+    return render(request, "encyclopedia/title.html", {
+        "title": title,
+        "description":description,
+        "page_available":page_available
+    })
